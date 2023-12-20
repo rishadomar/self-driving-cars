5aafa2d19d6c469a84ab85fa92cfe1422faa12d5
Rishad Omar <rishad@aux.co.za>
2023-12-20 18:35:26 +0200

    Remove points and drag them around

--- src/graphEditor.ts
+++ src/graphEditor.ts
@@ -4,6 +4,7 @@ class GraphEditor {
     ctx: CanvasRenderingContext2D;
     selected: Point | null;
     hovered: Point | null;
+    dragging: boolean;
 
     constructor(canvas: HTMLCanvasElement, graph: Graph) {
         this.canvas = canvas;
@@ -11,24 +12,52 @@ class GraphEditor {
         this.ctx = this.canvas.getContext('2d')!;
         this.selected = null;
         this.hovered = null;
+        this.dragging = false;
 
         this.addEventListeners();
     }
 
     addEventListeners() {
         this.canvas.addEventListener('mousedown', (e) => {
-            const x = e.offsetX;
-            const y = e.offsetY;
-            const point = new Point(x, y);
-
-            this.hovered = getNearestPoint(point, this.graph.points, 10);
-            if (this.hovered) {
-                this.selected = this.hovered;
+            if (e.button === 2) {
+                // right click
+                if (this.hovered) {
+                    this.graph.removePoint(this.hovered);
+                    if (this.selected === this.hovered) {
+                        this.selected = null;
+                    }
+                    this.hovered = null;
+                }
                 return;
             }
 
-            this.graph.addPoint(point);
-            this.selected = point;
+            if (e.button === 0) {
+                // left click
+                const x = e.offsetX;
+                const y = e.offsetY;
+                const point = new Point(x, y);
+
+                if (this.hovered) {
+                    this.selected = this.hovered;
+                    this.dragging = true;
+                    return;
+                }
+
+                this.graph.addPoint(point);
+                this.selected = point;
+                this.hovered = point;
+            }
+        });
+
+        this.canvas.addEventListener('mouseup', (e) => {
+            if (e.button === 0) {
+                // left click
+                this.dragging = false;
+            }
+        });
+
+        this.canvas.addEventListener('contextmenu', (e) => {
+            e.preventDefault();
         });
 
         this.canvas.addEventListener('mousemove', (e) => {
@@ -37,6 +66,10 @@ class GraphEditor {
             const point = new Point(x, y);
 
             this.hovered = getNearestPoint(point, this.graph.points, 10);
+            if (this.dragging && this.selected) {
+                this.selected.x = x;
+                this.selected.y = y;
+            }
         });
     }
 
